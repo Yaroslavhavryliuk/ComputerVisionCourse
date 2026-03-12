991112fdc2c9bbe47bca62099aaa0df17a201bd9
Yaroslavhavryliuk <hv.yarik@gmail.com>
2026-03-12 14:50:11 +0200

    Mirror webcam

--- Section1/Read_Image_Video_Webcam/read_webcam.py
+++ Section1/Read_Image_Video_Webcam/read_webcam.py
@@ -7,7 +7,8 @@ cap = cv2.VideoCapture(0)
 while True:
     ret, frame = cap.read()
     if ret:
-        cv2.imshow("Webcam", frame)
+        mir_frame  = cv2.flip(frame, 1)
+        cv2.imshow("Webcam", mir_frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
     else:
